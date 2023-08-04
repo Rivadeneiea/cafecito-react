044ac1e2237b2d8e5f91fbae7d01a53b6d13d352
Rivadeneiea <joserivadeneira538@gmail.com>
2023-08-04 15:50:33 -0300

    agrego validacion

--- src/components/views/producto/EditarProducto.jsx
+++ src/components/views/producto/EditarProducto.jsx
@@ -1,40 +1,69 @@
 import { Form, Button } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 const EditarProducto = () => {
-  
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  // esta funcion pide logear al usuario
+  const onSubmit = (producto) => {
+    console.log("aca agrego logica");
+    console.log(producto);
+  };
   return (
     <section className="container mainSection">
       <h1 className="display-4 mt-5">Editar producto</h1>
       <hr />
-      <Form >
+      <Form onSubmit={handleSubmit(onSubmit)}>
         <Form.Group className="mb-3" controlId="formNombreProdcuto">
           <Form.Label>Producto*</Form.Label>
           <Form.Control
             type="text"
             placeholder="Ej: Cafe"
+            {...register("Producto", {
+              required: "ingrese un producto valido",
+            })}
           />
-         
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formNombreProdcuto">
+          <Form.Label>descripcion de producto</Form.Label>
+          <Form.Control
+            as="textarea"
+            type="text"
+            placeholder="Leave a comment here"
+            {...register("descripcionProducto", {
+              required: "ingrese un producto valido",
+            })}
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formPrecio">
           <Form.Label>Precio*</Form.Label>
           <Form.Control
             type="number"
             placeholder="Ej: 50"
+            {...register("precioProducto", {
+              required: "ingrese un precio valido",
+            })}
           />
-       
         </Form.Group>
         <Form.Group className="mb-3" controlId="formImagen">
           <Form.Label>Imagen URL*</Form.Label>
           <Form.Control
             type="text"
             placeholder="Ej: https://www.pexels.com/es-es/vans-en-blanco-y-negro-fuera-de-la-decoracion-para-colgar-en-la-pared-1230679/"
-      
+            {...register("imagenProducto", {
+              required: "ingrese una imagen",
+            })}
           />
-      
         </Form.Group>
         <Form.Group className="mb-3" controlId="formPrecio">
           <Form.Label>Categoria*</Form.Label>
           <Form.Select
+            {...register("categoriaProducto", {
+              required: "ingrese una categoria valida",
+            })}
           >
             <option value="">Seleccione una opcion</option>
             <option value="bebida caliente">Bebida caliente</option>
@@ -42,7 +71,6 @@ const EditarProducto = () => {
             <option value="dulce">Dulce</option>
             <option value="salado">Salado</option>
           </Form.Select>
-       
         </Form.Group>
         <Button variant="primary" type="submit">
           Guardar
