95e9a22360560f4804bd140c00d07dba054a97a0
Rivadeneiea <joserivadeneira538@gmail.com>
2023-08-15 11:24:20 -0300

    cambio selector categoria

--- src/components/views/Administrador.jsx
+++ src/components/views/Administrador.jsx
@@ -1,33 +1,34 @@
 import { Table, Button } from "react-bootstrap";
 import ItemProducto from "./producto/ItemProducto";
+import { Link } from "react-router-dom";
 
 const Administrador = () => {
-    return (
-        <section className="container mainSection">
-        <div className="d-flex justify-content-between align-items-center mt-5">
-          <h1 className="display-4 ">Productos disponibles</h1>
-          <Button className="btn btn-primary" to='/administrar/crear'>
-            Agregar
-          </Button>
-        </div>
-        <hr />
-        <Table responsive striped bordered hover>
-          <thead>
-            <tr>
-              <th>Cod</th>
-              <th>Producto</th>
-              <th>Precio</th>
-              <th>URL de Imagen</th>
-              <th>Categoria</th>
-              <th>Opciones</th>
-            </tr>
-          </thead>
-          <tbody>
-           <ItemProducto></ItemProducto>
-          </tbody>
-        </Table>
-      </section>
-    );
+  return (
+    <section className="container mainSection">
+      <div className="d-flex justify-content-between align-items-center mt-5">
+        <h1 className="display-4 ">Productos disponibles</h1>
+        <Link className="btn btn-primary" to="/administrador/crear">
+          Agregar
+        </Link>
+      </div>
+      <hr />
+      <Table responsive striped bordered hover>
+        <thead>
+          <tr>
+            <th>Cod</th>
+            <th>Producto</th>
+            <th>Precio</th>
+            <th>URL de Imagen</th>
+            <th>Categoria</th>
+            <th>Opciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          <ItemProducto></ItemProducto>
+        </tbody>
+      </Table>
+    </section>
+  );
 };
 
 export default Administrador;
